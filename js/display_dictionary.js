7eeb66b69e87019345597eccb0801ca55af94af9
Tim Hosgood <timhosgood@gmail.com>
2020-10-21 00:48:37 +0200

    language selection

--- js/display_dictionary.js
+++ js/display_dictionary.js
@@ -1,26 +1,45 @@
 const langs = ["CA","DE","EN","ES","FR","IT","JA","PT","RU","ZH"]
+var visible_langs = ["EN"];
+var dict = {};
 
-$(document).ready(function() {
+var update_table = function(data = dict) {
+  $("#table_body").empty();
+  $("<tr id=\"table_headers\"></tr>").appendTo("#table_body")
+
   var table_headers = "";
-  $.each(langs, function(l, lang) {
+  $.each(visible_langs, function(l, lang) {
     table_headers += "<th>";
     table_headers += lang;
     table_headers += "</th>";
   });
-  $(table_headers).appendTo("#table_headers");
+  $(table_headers).appendTo("#table_headers")
 
-  $.getJSON("https://thosgood.com/maths-dictionary/nouns.json", function(json) {
-    $.each(json, function(i, item) {
-      var row = "<tr>";
-      $.each(langs, function(l, lang) {
-        row += "<td>";
-        if (item["root"][lang]["atom"]) {
-          row += item["root"][lang]["atom"];
-        }
-        row += "</td>";
-      });
-      row += "</tr>";
-      $(row).appendTo("#table_body");
+  $.each(data, function(i, item) {
+    var row = "<tr>";
+    $.each(visible_langs, function(l, lang) {
+      row += "<td>";
+      if (typeof item["root"][lang] !== 'undefined') {
+        row += item["root"][lang]["atom"];
+      };
+      row += "</td>";
     });
+    row += "</tr>";
+    $(row).appendTo("#table_body");
   });
+};
+
+$(document).ready(function() {
+  $.getJSON("https://thosgood.com/maths-dictionary/nouns.json", function(json) {
+    dict = json;
+    update_table(dict);
+  })
+});
+
+$("input").on("click", function() {
+  visible_langs = [];
+  var checked = $("input:checked");
+  $.each(checked, function(i, item){
+    visible_langs.push(item.name);
+  });
+  update_table();
 });
