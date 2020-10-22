b5559a0a690e785bb421c77ada5b6d6abbfe1c2d
Tim Hosgood <timhosgood@gmail.com>
2020-10-23 01:38:35 +0200

    factored out adjectives

--- website/js/display_dictionary.js
+++ website/js/display_dictionary.js
@@ -5,7 +5,7 @@ var dict = {};
 
 // TODO: sorting (https://stackoverflow.com/a/19947532/2352867)
 
-var update_table = function(data = dict) {
+var updateTable = function(data = dict) {
 
   $("#table_body").empty();
   $("<tr id=\"table_headers\"></tr>").appendTo("#table_body")
@@ -21,7 +21,7 @@ var update_table = function(data = dict) {
   $.each(data, function(i, item) {
 
     var emptyEntryRow = true;
-    var entryRow = "<tr class=\"noun\">";
+    var entryRow = `<tr class="noun" id=${i}>`;
 
     $.each(visible_langs, function(l, lang) {
 
@@ -50,42 +50,50 @@ var update_table = function(data = dict) {
       $(entryRow).appendTo("#table_body");
     };
 
-    // skip to next item in data if this entry doesn't have any adjectives
-    if (typeof item["adjs"] === "undefined" || item["adjs"] === "") {
-      return true;
-    };
-
-    // TODO: move this out to a separate function that gets called when we click
-    //       on a row: it will take the entry of that row as an argument
-    // do the same as we're currently doing with nouns, but now with adjectives
-    $.each(item["adjs"], function(a, adj) {
-      var emptyAdjRow = true;
-      var adjRow = "<tr class=\"adjective\">";
-      $.each(visible_langs, function(l, lang) {
-        adjRow += "<td>";
-        var adjective = adj[lang]
-        if (typeof adjective === "undefined" || adjective["atom"] === "") {
-          adjRow += "</td>";
-          return true;
-        };
-        emptyAdjRow = false;
-        if (adjective["pstn"] === "after"){adjRow+="___ "};
-        adjRow += adjective["atom"];
-        if (adjective["pstn"] === "before"){adjRow+=" ___"};
-        adjRow += "</td>";
-      }); // end $.each(visible_langs)
-      adjRow += "</tr>";
-      if (!emptyAdjRow){$(adjRow).appendTo("#table_body");}
-    }); // end $.each(item["adjs"])
+    $.each(showAdjectives(i), function(a, adj) {
+      $(adj).appendTo("#table_body");
+    });
 
   }); // end $.each(data)
+};
+
+var showAdjectives = function (nounID) {
+  var item = dict[nounID];
+  var adjRows = [];
+
+  if (typeof item["adjs"] === "undefined" || item["adjs"] === "") {
+    return false;
+  };
+
+  $.each(item["adjs"], function(a, adj) {
+    var adjRow = "<tr class=\"adjective\">";
+    var emptyAdjRow = true;
+    $.each(visible_langs, function(l, lang) {
+      adjRow += "<td>";
+      var adjective = adj[lang]
+      if (typeof adjective === "undefined" || adjective["atom"] === "") {
+        adjRow += "</td>";
+        return true;
+      };
+      emptyAdjRow = false;
+      if (adjective["pstn"] === "after"){adjRow+="___ "};
+      adjRow += adjective["atom"];
+      if (adjective["pstn"] === "before"){adjRow+=" ___"};
+      adjRow += "</td>";
+    }); // end $.each(visible_langs)
+    adjRow += "</tr>";
+    if (!emptyAdjRow) {
+      adjRows.push(adjRow);
+    }
+  }); // end $.each(item["adjs"])
+  return adjRows;
 };
 
 
 $(document).ready(function() {
   $.getJSON("https://thosgood.com/maths-dictionary/nouns.json", function(json) {
     dict = json;
-    update_table(dict);
+    updateTable(dict);
   })
 });
 
@@ -96,5 +104,5 @@ $("input").on("click", function() {
   $.each(checked, function(i, item){
     visible_langs.push(item.name);
   });
-  update_table();
+  updateTable();
 });
