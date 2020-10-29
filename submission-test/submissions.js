2c5257d4819ffaee10637c55825b51fceef0bfa7
Tim Hosgood <timhosgood@gmail.com>
2020-10-29 01:07:26 +0100

    vague start

--- submission-test/submissions.js
+++ submission-test/submissions.js
@@ -1,6 +1,7 @@
 var sourceLangs = [];
-var targetLangs = [];
+var targetLang = "";
 var dict = {};
+var needingTranslation = [];
 
 $(document).ready(function() {
   $.getJSON("https://thosgood.com/maths-dictionary/nouns.json", function(json) {
@@ -9,10 +10,24 @@ $(document).ready(function() {
 });
 
 $(document).on("click", "#start", function(obj) {
+  sourceLangs = [];
   $("#from_language_selectors input:checked").each( function(i, item){
     sourceLangs.push(item.name);
   });
-  $("#to_language_selectors input:checked").each( function(i, item){
-    targetLangs.push(item.name);
+  targetLang = $("#to_language").val();
+  $.each(dict, function(i, item) {
+    $.each(sourceLangs, function(l, lang) {
+      if (typeof item["root"][lang] !== undefined) {
+        var sourceAtom = item["root"][lang]["atom"];
+        var targetAtom = item["root"][targetLang]["atom"]
+        if (sourceAtom !== "" && targetAtom === "") {
+          // TODO: if the atom exists in ALL source languages then this should
+          //       only make ONE entry in needingTranslation
+          var entry = { "id": i, "language": lang, "atom": sourceAtom }
+          needingTranslation.push(entry);
+        };
+      };
+    });
   });
+  console.log(needingTranslation);
 });
