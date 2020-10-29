aa90125cf25ad899aa43c05f6868efedc7517221
Tim Hosgood <timhosgood@gmail.com>
2020-10-29 01:28:01 +0100

    multiple languages

--- submission-test/submissions.js
+++ submission-test/submissions.js
@@ -10,24 +10,30 @@ $(document).ready(function() {
 });
 
 $(document).on("click", "#start", function(obj) {
+  needingTranslation = [];
   sourceLangs = [];
+  targetLang = "";
+
   $("#from_language_selectors input:checked").each( function(i, item){
     sourceLangs.push(item.name);
   });
+
   targetLang = $("#to_language").val();
+
   $.each(dict, function(i, item) {
+    var entry = { "id": i, "existing": {} };
     $.each(sourceLangs, function(l, lang) {
       if (typeof item["root"][lang] !== undefined) {
         var sourceAtom = item["root"][lang]["atom"];
         var targetAtom = item["root"][targetLang]["atom"]
         if (sourceAtom !== "" && targetAtom === "") {
-          // TODO: if the atom exists in ALL source languages then this should
-          //       only make ONE entry in needingTranslation
-          var entry = { "id": i, "language": lang, "atom": sourceAtom }
-          needingTranslation.push(entry);
+          entry["existing"][lang] = sourceAtom;
         };
       };
     });
+    if (!jQuery.isEmptyObject(entry["existing"])) {
+      needingTranslation.push(entry);  
+    };
   });
   console.log(needingTranslation);
 });
