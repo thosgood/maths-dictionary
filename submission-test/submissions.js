34f2e5f1617343f013e949427fb6425fafb0d5cb
Tim Hosgood <timhosgood@gmail.com>
2020-10-30 03:16:19 +0100

    some refactoring

--- submission-test/submissions.js
+++ submission-test/submissions.js
@@ -3,6 +3,7 @@ var targetLang = "";
 var targetLangName = "";
 var dict = {};
 var needingTranslation = [];
+var questions = [];
 
 
 
@@ -42,131 +43,116 @@ $(document).on("click", "#start", function() {
     };
   });
 
-  var questions = generateQuestions(needingTranslation);
-  $.each(questions, function(q, question) {
-    // TODO: these should also be in the target language!
-    questionCard = `
-<div class="question_card">
-  ${question}
-  <ul class="question_card_buttons">
-    <li><button name="skip" class="skip">Skip</button></li>
-    <li><button name="finished" class="finished">Finish now</button></li>
-    <li><button name="next" class="next">Next</button></li>
-  </ul>
-  <span class="question_number">${q+1}/${questions.length}</span>
-</div>`
-    $(questionCard).appendTo("#questions");
-  });
+  var questionCard = generateQuestionCard(targetLang, needingTranslation.length);
 
   // TODO: give some message in the case where question is empty!
   $("#initial_options").css("display", "none");
-  // $("#questions").append(`<input type="submit" id="submit" value="Submit">`)
+  $(questionCard).appendTo("#questions");
+
+  var firstUnknown = needingTranslation[0];
+  var foreignContent = [];
+  $.each(firstUnknown["existing"], function(u, unknown) {
+    var foreignWord =`<span class="unknown">${unknown}<span class="tooltip">${u}</span></span>`;
+    foreignContent.push(foreignWord);
+  });
+  $("#foreign").html(foreignContent.join(' /'));
 });
 
 
 
-var generateQuestions = function(toTranslate) {
-  var questions = [];
-  var foreign = "";
-  $.each(toTranslate, function(e, entry) {
-    var sources = [];
-    $.each(entry["existing"], function(s, source) {
-      foreign = `
-<div class="foreign">
-  <span class="unknown">${source}</span>
-  <span class="tooltip">${s}</span>
-</div>`;
-      sources.push(foreign);
-    });
+var generateQuestionCard = function(targetLang, totalNum) {
+  var foreign = `<div id="foreign"></div>`;
+  var questionLabel = "";
 
-    var question = `<label for="${entry["id"]}">`;
-    switch(targetLang) {
-      case "CA":
-        question += `Com es diu ${sources.join(" /")} en català?`;
-        break;
-      case "DE":
-        question += `Wie sagt man auf  Deutsch ${sources.join(" /")}?`;
-        break;
-      case "EN":
-        question += `How do you say ${sources.join(" /")} in English?`;
-        break;
-      case "ES":
-        question += `¿Cómo se dice ${sources.join(" /")} en español?`;
-        break;
-      case "FA":
-        // TODO: .reverse.join(" "), but before .join(" /")
-        question += `چگونه به فارسی ${sources.join(" /")} می گویید؟`;
-        break;
-      case "FI":
-        question += `Kuinka sanot ${sources.join(" /")} suomeksi?`;
-        break;
-      case "FR":
-        question += `Comment dit-on ${sources.join(" /")} en français?`;
-        break;
-      case "IT":
-        question += `Come se dice ${sources.join(" /")} in italiano?`;
-        break;
-      case "JA":
-        question += `日本語で ${sources.join(" /")} は何と言いますか?`;
-        break;
-      case "KO":
-        question += `한국어로 ${sources.join(" /")}이 뭐야?`;
-        break;
-      case "PL":
-        question += `Jak powiedzieć ${sources.join(" /")} po polsku?`;
-        break;
-      case "PT":
-        question += `Como se diz ${sources.join(" /")} em português?`;
-        break;
-      case "RU":
-        question += `Как сказать ${sources.join(" /")} по-русски?`;
-        break;
-      case "TR":
-        question += `Türkçe'de ${sources.join(" /")} nasıl denir?`;
-        break;
-      case "ZH":
-        question += `${sources.join(" /")} 中文怎么说?`;
-        break;
-      default:
-        question = `How do you say ${sources.join(" /")} in English?`;
-        break;
-    }
-    question += "</label>";
-    question += `\n<input type="text" id="${entry["id"]}" name="${entry["id"]}">`;
-    questions.push(question);
-  });
+  switch(targetLang) {
+    case "CA":
+      questionLabel = `Com es diu ${foreign} en català?`;
+      break;
+    case "DE":
+      questionLabel = `Wie sagt man auf  Deutsch ${foreign}?`;
+      break;
+    case "EN":
+      questionLabel = `How do you say ${foreign} in English?`;
+      break;
+    case "ES":
+      questionLabel = `¿Cómo se dice ${foreign} en español?`;
+      break;
+    case "FA":
+      // TODO: .reverse.join(" "), but before .join(" /")
+      questionLabel = `چگونه به فارسی ${foreign} می گویید؟`;
+      break;
+    case "FI":
+      questionLabel = `Kuinka sanot ${foreign} suomeksi?`;
+      break;
+    case "FR":
+      questionLabel = `Comment dit-on ${foreign} en français?`;
+      break;
+    case "IT":
+      questionLabel = `Come se dice ${foreign} in italiano?`;
+      break;
+    case "JA":
+      questionLabel = `日本語で ${foreign} は何と言いますか?`;
+      break;
+    case "KO":
+      questionLabel = `한국어로 ${foreign}이 뭐야?`;
+      break;
+    case "PL":
+      questionLabel = `Jak powiedzieć ${foreign} po polsku?`;
+      break;
+    case "PT":
+      questionLabel = `Como se diz ${foreign} em português?`;
+      break;
+    case "RU":
+      questionLabel = `Как сказать ${foreign} по-русски?`;
+      break;
+    case "TR":
+      questionLabel = `Türkçe'de ${foreign} nasıl denir?`;
+      break;
+    case "ZH":
+      questionLabel = `${foreign} 中文怎么说?`;
+      break;
+    default:
+      questionLabel = `How do you say ${foreign} in English?`;
+      break;
+  }
 
-  return questions;
+  questionCard = `
+<div id="question_card">
+  <label for="labelFor">
+    ${questionLabel}
+  </label>
+  <input type="text" id="inputID" name="inputName">
+  <ul id="question_card_buttons">
+    <li><button name="skip" id="skip">Skip</button></li>
+    <li><button name="finished" id="finished">Finish now</button></li>
+    <li><button name="next" id="next">Next</button></li>
+  </ul>
+  <span id="question_number">1/${totalNum}</span>
+</div>`
+
+  return questionCard;
 };
 
 
 
-$(document).on("click", ".skip", function() {
+$(document).on("click", "#skip", function() {
   // TODO
   console.log("skip");
 });
 
 
 
-$(document).on("click", ".finished", function() {
+$(document).on("click", "#finished", function() {
   // TODO
   console.log("finished");
 });
 
 
 
-$(document).on("click", ".next", function() {
+$(document).on("click", "#next", function() {
   // TODO
   // TODO: get the value inside the input "just above"
   // TODO: disable this button unless the input is non-empty
-  var value = $(this).closest("div.question_card").find("input").val();
+  var value = $(this).closest("div#question_card").find("input").val();
   console.log(value);
 });
-
-
-
-// $("form#questions").submit(function(event) {
-//   var submissionData = $("form#questions").serializeArray();
-//   // submissionData["language"] = targetLang;
-//   event.preventDefault();
-// });
