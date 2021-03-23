38d7505f68c0e728bba322cb2d04d53637faca36
Tim Hosgood <timhosgood@gmail.com>
2021-03-23 02:39:30 +0000

    fixed error

--- website/display_dictionary.js
+++ website/display_dictionary.js
@@ -96,7 +96,6 @@ $(document).ready(function() {
       data: data,
       "columns": columnsConf,
       "autoWidth": false,
-      fixedHeader: true,
       "paging": true,
       "pageLength": 10,
       "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
