8459a9ec1b88e00f6188dfc5b4dc39e1ac7c238b
Tim Hosgood <timhosgood@gmail.com>
2023-05-26 21:27:21 +0200

    longer default page length

--- website/display_dictionary.js
+++ website/display_dictionary.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
       "columns": columnsConf,
       "autoWidth": false,
       "paging": true,
-      "pageLength": 10,
+      "pageLength": 50,
       "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
       // "pagingType": "numbers",
       "order": [[ 0, "asc" ]],
