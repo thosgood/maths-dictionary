da0a56917b9d03c55671bf533702961549527fda
Tim Hosgood <timhosgood@gmail.com>
2023-05-26 21:27:35 +0200

    (ok a bit shorter)

--- website/display_dictionary.js
+++ website/display_dictionary.js
@@ -114,7 +114,7 @@ $(document).ready(function() {
       "columns": columnsConf,
       "autoWidth": false,
       "paging": true,
-      "pageLength": 50,
+      "pageLength": 25,
       "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
       // "pagingType": "numbers",
       "order": [[ 0, "asc" ]],
